a89f198d07683d4cf1459aabe1eb2b4c7bc35bcb
IacobSon <dorian.iacobescu@gmail.com>
2019-05-07 09:40:26 +0200

    remove unused controller

--- lib/bluecode_connector_web/controllers/clearing/payment_controller.ex
+++
@@ -1,7 +0,0 @@
-defmodule BluecodeConnectorWeb.Clearing.PaymentController do
-  use BluecodeConnectorWeb, :controller
-
-  def create(conn, params) do
-    IO.puts ~s(\n\n!!! params: #{inspect params}\n)
-  end
-end
