074e2970bdb14c6fafbd1ea39e178022775e39ce
Sebastian Burkhard <s.burkhard@bluecode.com>
2019-05-07 12:28:53 +0700

    add a merchant_iban for the demo in case non was sent.

--- lib/bluecode_connector_web/controllers/clearing/clearing_controller.ex
+++ lib/bluecode_connector_web/controllers/clearing/clearing_controller.ex
@@ -47,13 +47,23 @@ defmodule BluecodeConnectorWeb.ClearingApi.ClearingController do
 
   # Calls the PIS payment endpoint
   defp post_instant_payment(%Account{} = acct, params) do
+    # Fake it till you make it.... Merchant IBAN is sent to the BC Payment Gateway
+    # by the acquirer and forwarded to this clearing API implementation.
+    #
+    # In case we don't make it on time, we just take a hard-coded sample IBAN.
+    # In reality not receiving a merchant_iban would mean that this payment is not
+    # instant payment capable and has to go through the normal bluecode scheme.
+
+    merchant_iban = params["merchant_iban"] || "DE95500105176345611221"
+
     params = %{
       debtorAccount: %{iban: acct.iban},
-      creditorAccount: %{iban: params["merchant_iban"]},
+      creditorAccount: %{iban: merchant_iban},
       instructedAmount: %{currency: "EUR", amount: params["amount"]},
       creditorName: params["merchant_name"],
       endToEndIdentification: params["bc_tx_id"],
-      remittanceInformationUnstructured: params["merchant_tx_id"]
+      #
+      remittanceInformationUnstructured: params["acquirer_tx_id"]
     }
 
     {:ok, %{body: body}} =
